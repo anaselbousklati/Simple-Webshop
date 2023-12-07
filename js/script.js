c6172ead64283c33c6218942c9256bca9aa4f619
Anas El Bousklati <74158385+anaselbousklati@users.noreply.github.com>
2023-12-07 11:36:58 +0100

    orderd

---
+++ js/script.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", function () {
+    if (!sessionStorage.getItem('cartItems')) {
+        sessionStorage.setItem('cartItems', JSON.stringify([]));
+    }
+
+    const productsData = [
+        { imageSrc: "/img/AirPod 2nd Gen.jpg", title: "AirPod 2nd Gen", price: "€129" },
+        { imageSrc: "/img/apple-watch-pcq.jpg", title: "Apple Watch", price: "€299" },
+        { imageSrc: "/img/iPhone-14.jpg", title: "iPhone 14", price: "€999" },
+        { imageSrc: "/img/ipad-pro.jpg", title: "iPan Pro", price: "€629" },
+        { imageSrc: "/img/AirPod 2nd Gen.jpg", title: "AirPod 2nd Gen", price: "€129" },
+        { imageSrc: "/img/apple-watch-pcq.jpg", title: "Apple Watch", price: "€299" },
+        { imageSrc: "/img/iPhone-14.jpg", title: "iPhone 14", price: "€999" },
+        { imageSrc: "/img/ipad-pro.jpg", title: "iPan Pro", price: "€629" }
+    ];
+
+    const allProductsContainer = document.getElementById('all-products');
+
+    function addToCart(productData) {
+        const cartItems = JSON.parse(sessionStorage.getItem('cartItems'));
+
+        const existingCartItem = cartItems.find(item => item.title === productData.title);
+
+        if (existingCartItem) {
+            existingCartItem.quantity += 1;
+        } else {
+            cartItems.push({
+                title: productData.title,
+                price: productData.price,
+                quantity: 1
+            });
+        }
+
+        sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+        updateTotalPrice();
+    }
+
+    function updateTotalPrice() {
+        const cartItems = JSON.parse(sessionStorage.getItem('cartItems')) || [];
+        let totalPrice = 0;
+
+        cartItems.forEach(item => {
+            const price = parseFloat(item.price.slice(1));
+            const quantity = item.quantity || 0;
+            totalPrice += price * quantity;
+        });
+
+        document.getElementById('total-price').textContent = `€${totalPrice.toFixed(2)}`;
+    }
+
+    function createProductElement(productData) {
+        const productContainer = document.createElement('div');
+        productContainer.classList.add('product');
+
+        const productImage = document.createElement('img');
+        productImage.src = productData.imageSrc;
+
+        const productInfoContainer = document.createElement('div');
+        productInfoContainer.classList.add('product-info');
+
+        const productTitle = document.createElement('h4');
+        productTitle.classList.add('product-title');
+        productTitle.textContent = productData.title;
+
+        const productPrice = document.createElement('p');
+        productPrice.classList.add('product-price');
+        productPrice.textContent = productData.price;
+
+        const buyNowButton = document.createElement('button');
+        buyNowButton.classList.add('product-btn');
+        buyNowButton.textContent = 'Koop nu';
+
+        buyNowButton.addEventListener('click', function () {
+            addToCart(productData);
+        });
+
+        productInfoContainer.appendChild(productTitle);
+        productInfoContainer.appendChild(productPrice);
+        productInfoContainer.appendChild(buyNowButton);
+
+        productContainer.appendChild(productImage);
+        productContainer.appendChild(productInfoContainer);
+
+        allProductsContainer.appendChild(productContainer);
+    }
+
+    productsData.forEach(createProductElement);
+});
